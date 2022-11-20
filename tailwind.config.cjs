6a11c3f7c90b64a0ba0854325252236af56c7ffa
Sadiq Salau <sadiqsalau888@gmail.com>
2022-11-20 21:42:24 +0100

    Extended spacing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        2.5: "0.625rem",
+      },
+    },
   },
   plugins: [],
 };
