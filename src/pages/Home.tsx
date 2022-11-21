c0ce92a41eee30af0aa6541cd03e79c7d34b3611
Sadiq Salau <sadiqsalau888@gmail.com>
2022-11-21 18:27:25 +0100

    Implemented querying and display of repositories as apps

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,50 @@
+import { RestEndpointMethodTypes } from "@octokit/rest";
+import { useQuery } from "@tanstack/react-query";
+import { appDomainPath, appName } from "../core/helpers";
+import octokit from "../core/octokit";
+import Header from "../layouts/Header";
+
+interface AppProps {
+  repo: RestEndpointMethodTypes["repos"]["listForOrg"]["response"]["data"][number];
+}
+
 function Home() {
-  return <>Hello World!</>;
+  const { data, isLoading, isSuccess } = useQuery(["list_org_repos"], () =>
+    octokit.repos.listForOrg({
+      org: import.meta.env.VITE_ORG_NAME,
+    })
+  );
+  return (
+    <div>
+      <Header />
+      <div className="container mx-auto p-2.5">
+        <h1 className="font-bold px-2">Apps</h1>
+        {isLoading && <p className="text-center">Loading</p>}
+        {isSuccess && (
+          <div className="py-3">
+            {data?.data.map((repo, i) => (
+              <App key={repo.name} repo={repo} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function App({ repo }: AppProps) {
+  return (
+    <div>
+      {/* App Icon */}
+      <img
+        src={appDomainPath(repo.name, "/icon@192.png")}
+        alt={repo.name}
+        className="w-16 h-16 rounded-lg"
+      />
+      {/* App Name */}
+      <div className="font-bold text-xs w-full">{appName(repo.name)}</div>
+    </div>
+  );
 }
 
 export default Home;
