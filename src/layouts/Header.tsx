1f6146fb30ef5fc6d2b9f37178d3a39850bf8022
Sadiq Salau <sadiqsalau888@gmail.com>
2022-11-22 09:02:58 +0100

    Changed font weight to bold

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="container mx-auto p-2.5 flex justify-center">
+    <div className="container mx-auto p-2.5 flex font-bold justify-center">
       <h1>{import.meta.env.VITE_APP_NAME}</h1>
     </div>
   );
