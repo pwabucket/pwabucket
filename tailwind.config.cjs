14d050fa4ac48496392c55172c646b96086eebc1
Sadiq Salau <sadiqsalau888@gmail.com>
2022-11-22 09:03:56 +0100

    Configured Urbanist font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Urbanist", ...defaultTheme.fontFamily.sans],
+      },
       spacing: {
         2.5: "0.625rem",
       },
