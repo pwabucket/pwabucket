060fd45a5823d45cfd17f76a4c6849164e4775ca
Sadiq Salau <sadiqsalau888@gmail.com>
2022-11-23 08:53:05 +0100

    Sticky header with shadow

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,12 +1,14 @@
 function Header() {
   return (
-    <div className="container mx-auto p-2.5 flex font-bold justify-center items-center gap-2">
-      <img
-        src="/icon@192.png"
-        className="w-10 h-10"
-        alt={import.meta.env.VITE_APP_NAME}
-      />
-      <h1>{import.meta.env.VITE_APP_NAME}</h1>
+    <div className="sticky top-0 bg-white shadow">
+      <div className="container mx-auto p-2.5 flex font-bold justify-center items-center gap-2">
+        <img
+          src="/icon@192.png"
+          className="w-10 h-10"
+          alt={import.meta.env.VITE_APP_NAME}
+        />
+        <h1>{import.meta.env.VITE_APP_NAME}</h1>
+      </div>
     </div>
   );
 }
