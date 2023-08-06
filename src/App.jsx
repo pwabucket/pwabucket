c1384bec0f2accf1bb3a73892eb69100e1aa73e9
Sadiq Salau <sadiqsalau888@gmail.com>
2023-08-06 21:18:33 +0100

    Fetch and display apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,26 @@
-export const App = () => null;
+import clsx from "clsx";
+import { capitalCase } from "change-case";
+import { useQuery } from "@tanstack/react-query";
+
+export const App = () => {
+  const { isLoading, data } = useQuery(["listApps"]);
+
+  return (
+    <div className="container px-4 mx-auto">
+      <div
+        className={clsx(
+          "grid gap-2 grid-cols-[repeat(auto-fill,_minmax(theme(spacing.28),_1fr))]"
+        )}
+      >
+        {isLoading
+          ? "Loading..."
+          : data.items.map((data) => (
+              <div key={data.id} className="flex flex-col gap-2">
+                <img src={`${data.homepage}logo.svg`} alt={data.name} />
+                <h1>{capitalCase(data.name)}</h1>
+              </div>
+            ))}
+      </div>
+    </div>
+  );
+};
