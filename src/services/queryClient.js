35b70b1e8748eff13695a58138ff13fe038c5659
Sadiq Salau <sadiqsalau888@gmail.com>
2023-08-06 21:30:33 +0100

    Added comments

--- src/services/queryClient.js
+++ src/services/queryClient.js
@@ -5,7 +5,14 @@ import { api } from "./api";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      /**
+       * Call the api service with the query key data
+       */
       queryFn: ({ queryKey }) => api[queryKey[0]](...queryKey.slice(1)),
+
+      /**
+       * Select the "data" property
+       */
       select: ({ data }) => data,
     },
   },
