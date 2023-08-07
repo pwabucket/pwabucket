dec74729641f71cf33c5b4dedaac99a56c9bdaca
Sadiq Salau <sadiqsalau888@gmail.com>
2023-08-07 08:58:04 +0100

    Display app placeholder
    - Replaced loading text with placeholder
    - Increased spacing between image and name
    - Set name and description line height to 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import repeat from "repeat-element";
 import { capitalCase } from "change-case";
 import { useQuery } from "@tanstack/react-query";
 
@@ -6,14 +7,26 @@ export const App = () => {
   const { isLoading, data } = useQuery(["listApps"]);
 
   return (
-    <div className="container px-4 mx-auto">
+    <div className="container p-4 mx-auto">
       <div
         className={clsx(
-          "grid gap-2 grid-cols-[repeat(auto-fill,_minmax(theme(spacing.28),_1fr))]"
+          "grid gap-3",
+          "[--app-grid-size:theme(spacing.24)]",
+          "md:[--app-grid-size:theme(spacing.28)]",
+          "grid-cols-[repeat(auto-fill,_minmax(var(--app-grid-size),_1fr))]"
         )}
       >
         {isLoading
-          ? "Loading..."
+          ? repeat(undefined, 10).map((_, i) => (
+              <div key={i} className="flex flex-col gap-1">
+                {/* Placeholder Image */}
+                <div className="w-full aspect-square rounded-3xl mb-3 bg-neutral-100"></div>
+                {/* Placeholder Title */}
+                <div className="w-full h-3 bg-neutral-100"></div>
+                {/* Placeholder Description */}
+                <div className="w-full h-2 bg-neutral-100"></div>
+              </div>
+            ))
           : data.items.map((data) => (
               <a
                 key={data.id}
@@ -21,20 +34,20 @@ export const App = () => {
                 className="flex flex-col gap-1"
                 title={`${capitalCase(data.name)} - ${data.description}`}
               >
-                {/* Icon */}
+                {/* App Icon */}
                 <img
                   src={data.homepage + "pwa-192x192.png"}
                   alt={capitalCase(data.name)}
-                  className="w-full aspect-square rounded-3xl"
+                  className="w-full aspect-square rounded-3xl mb-3"
                 />
 
-                {/* Name */}
-                <h1 className="font-bold text-center">
+                {/* App Name */}
+                <h1 className="font-bold truncate text-center leading-none">
                   {capitalCase(data.name)}
                 </h1>
 
-                {/* Description */}
-                <p className="text-center truncate text-sm text-stone-600">
+                {/* App Description */}
+                <p className="text-center truncate text-sm leading-none text-stone-600">
                   {data.description}
                 </p>
               </a>
