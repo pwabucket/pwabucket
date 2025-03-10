b98c54b6758fd8d481e8bf828cdb7bafb143b288
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-10 10:45:51 +0100

    Search with topic

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import { octokit } from "./octokit";
 export const api = {
   listApps: () =>
     octokit.search.repos({
-      q: `props.PWA_IS_PUBLISHED:true org:${import.meta.env.VITE_OCTOKIT_ORG}`,
+      q: `pwa in:topics org:${import.meta.env.VITE_OCTOKIT_ORG}`,
     }),
 };
