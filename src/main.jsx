b166414cf655c41fcf24e6b0e7c8a7c310d2ffef
Sadiq Salau <sadiqsalau888@gmail.com>
2025-05-15 16:17:28 +0100

    Switched to Browser Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router";
+import { BrowserRouter } from "react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/services/queryClient.js";
 import { registerSW } from "virtual:pwa-register";
@@ -13,9 +13,9 @@ registerSW({ immediate: true });
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
