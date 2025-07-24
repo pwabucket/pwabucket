791547b9a2caecbbe0d6001ab4902ab6b6869cb2
Sadiq Salau <sadiqsalau888@gmail.com>
2025-07-24 06:44:13 +0100

    feat: Fetch updated data from repo in production

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,14 @@
 import axios from "axios";
 
 export const api = {
-  listApps: () => axios.get("/data/pwa-data.json").then((res) => res.data),
+  listApps: () =>
+    axios
+      .get(
+        import.meta.env.DEV
+          ? "/data/pwa-data.json"
+          : `https://raw.githubusercontent.com/${
+              import.meta.env.VITE_APP_ORG
+            }/${import.meta.env.VITE_APP_ID}/main/public/data/pwa-data.json`
+      )
+      .then((res) => res.data),
 };
