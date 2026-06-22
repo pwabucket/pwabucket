5abcbf8959953861382e9ddf287646f9af835084
Sadiq Salau <sadiqsalau888@gmail.com>
2026-06-22 14:12:45 +0100

    feat: Upgraded packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import { imagetools } from "vite-imagetools";
 import { loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
